83319dfbd6cb747236417f02c84ab11a8b7dfb06
Alfonso Cristobal Varela Toro <avarelato@uoc.edu>
2026-05-01 14:14:17 +0200

    P2 cambios menus, animacion, accesibilidad

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-console.log('Hello world!');
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import plato2 from 'url:./images/espagueti-pesto.jpg?width=220';
@@ -8,13 +7,38 @@ import plato4 from 'url:./images/osobuco_milanesa.jpg?width=220';
 import plato5 from 'url:./images/pizza_fungi.jpg?width=220';
 import plato6 from 'url:./images/tiramisu.jpg?width=220';
 
-const platos = [plato1, plato2, plato3, plato4, plato5, plato6];
+const platos = [
+  {
+    src: plato1,
+    alt: 'Plato de macarrones a la boloñesa',
+  },
+  {
+    src: plato2,
+    alt: 'Plato de espaguetis al pesto',
+  },
+  {
+    src: plato3,
+    alt: 'Plato de lasaña',
+  },
+  {
+    src: plato4,
+    alt: 'Cazuela de ossobuco a la milanesa',
+  },
+  {
+    src: plato5,
+    alt: 'Pizza funghi',
+  },
+  {
+    src: plato6,
+    alt: 'Plato de tiramisú',
+  },
+];
 
 const anime = document.getElementById('plato-volante');
-console.log('Imagen Id: ' + anime.id);
 
 anime.addEventListener('animationiteration', () => {
   const aleatorio = Math.floor(Math.random() * platos.length);
-  anime.src = platos[aleatorio];
-  console.log('anime.src : ' + anime.src);
+  anime.src = platos[aleatorio].src;
+  anime.alt = platos[aleatorio].alt;
+  //console.log('anime.src : ' + anime.src);
 });
